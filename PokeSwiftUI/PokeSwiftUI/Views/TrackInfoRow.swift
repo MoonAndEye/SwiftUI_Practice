d2e351d5cfcb9646961e12c4c53102370215ab04
Marvin Lin <marvin_lin@cmoney.com.tw>
2020-03-11 00:12:19 +0800

    Toggle on cell frame height change

--- PokeSwiftUI/PokeSwiftUI/Views/TrackInfoRow.swift
+++ PokeSwiftUI/PokeSwiftUI/Views/TrackInfoRow.swift
@@ -17,6 +17,7 @@ import SwiftUI
 struct TrackInfoRow: View {
     
     @State var result: Result
+    @State var expended: Bool
     
     var body: some View {
         
@@ -37,9 +38,11 @@ struct TrackInfoRow: View {
                         .font(.subheadline)
                         .foregroundColor(.white)
                 }
-            }.padding(.top, 12)
+            }
+            .padding(.top, 12)
+            Spacer()
             
-            HStack {
+            HStack(spacing: expended ? 20 : 30) {
                 Spacer()
                 Button(action: {
                     print("Fav")
@@ -61,15 +64,25 @@ struct TrackInfoRow: View {
                 }
             }
             .padding(.bottom, 12)
+            .opacity(expended ? 1.0 : 0.0)
+            .frame(maxHeight: expended ? .infinity : 0)
         }
-        .background(Color.green)
         .frame(height: 120)
         .padding(.leading, 23)
         .padding(.trailing, 15)
+        .frame(height: expended ? 120.0 : 80.0)
         .background(
             RoundedRectangle(cornerRadius: 20)
-                .fill(Color.green)
+                .fill(LinearGradient(
+                    gradient: Gradient(colors: [.white, .green]),
+                    startPoint: .leading,
+                    endPoint: .trailing
+                ))
         )
+            .padding(.horizontal)
+            .onTapGesture {
+                self.expended.toggle()
+        }
     }
 }
 
@@ -78,7 +91,11 @@ struct TrackInfoRow_Previews: PreviewProvider {
 //    let result = Result(trackId: 1, trackName: "My Name", collectionName: "My Collection")
     
     static var previews: some View {
+        VStack {
+            TrackInfoRow(result: Result(trackId: 1, trackName: "My Name", collectionName: "My Collection"), expended: false)
+            TrackInfoRow(result: Result(trackId: 2, trackName: "2 Name", collectionName: "My Collection"), expended: true)
+            TrackInfoRow(result: Result(trackId: 3, trackName: "3 Name", collectionName: "3 Collection"), expended: false)
+        }
         
-        TrackInfoRow(result: Result(trackId: 1, trackName: "My Name", collectionName: "My Collection"))
     }
 }
